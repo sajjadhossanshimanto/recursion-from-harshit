96fa69fda00061fa308202f81e22d5c951a660ce
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-01 21:04:23 +0600

    range print

--- 2. oneToN.py
+++ 2. oneToN.py
@@ -5,4 +5,15 @@ def one_to_N(n):
     one_to_N(n-1)
     print(n, end=" ")
 
+def range_print(start, end):
+    # both end points are inclusive
+    if start>end: return
+    # end point is not included
+    # if start==end: return
+
+    print(start, end=" ")
+    range_print(start+1, end)
+
 one_to_N(5)
+print()
+range_print(6, 10)

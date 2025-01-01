2b8a3378b2b90afbcaa48a7afd8aa40de11edab8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-01 21:35:38 +0600

    notes upon complexity

--- 4. print array.py
+++ 4. print array.py
@@ -5,4 +5,7 @@ def print_list(l: list, index=0):
     print(l[index])
     print_list(l, index+1)
 
+# time complexity:              1* O(N)
+# auxilary space complexity:    O(N)
+
 print_list([1, 2, 3, 4, 5])

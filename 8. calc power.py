6df550c37c61cc5191df32bdaaa099159948e471
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-01 23:49:04 +0600

    power with btter time complexity

--- 8. calc power.py
+++ 8. calc power.py
@@ -1,8 +1,21 @@
 
 
-def power(n, p):
-    if p==0: return 1
-    return n*power(n, p-1)
+# def power(n, p):
+#     if p==0: return 1
+#     return n*power(n, p-1)
+
+# print(power(3, 6))
+
+def power(num, exp):# O(logN)
+    if exp==0: return 1
+    if exp==1: return num
+
+    half = power(num, exp//2)
+    if exp&1:# odd
+        return half*half*num
+    # even
+    return half*half
 
 print(power(2, 4))
-print(power(3, 5))
+print(power(3, 3))
+print(power(3, 6))

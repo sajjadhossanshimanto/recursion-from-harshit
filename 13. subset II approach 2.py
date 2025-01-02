c1e331a6d91af32b954d095f85b8b845fa403118
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-02 21:44:57 +0600

    lead code submission beats 100%

--- 13. subset II approach 2.py
+++ 13. subset II approach 2.py
@@ -24,3 +24,34 @@ l = ['a', 'b', 'b', 'c']
 s.sort()
 subsets(l)
 print(all_sub)
+
+
+#%%
+from typing import List
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        all_sub = []
+        def backtrack(index=0, temp = []):
+            '''sorting is must before passing to the function'''
+            
+            all_sub.append(temp[:])
+            
+            prev = None
+            for i in range(index, len(nums)):
+                if prev == nums[i]: continue
+                prev = nums[i]
+
+                temp.append(nums[i])
+                backtrack(i+1, temp)
+                temp.pop()
+        
+        backtrack()
+        return all_sub
+
+#%%
+Solution().subsetsWithDup([1, 2, 2])
+
+# %%

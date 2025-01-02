15d176e3f3e9eb679e84e32394251e19376f2d0e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-02 14:55:45 +0600

    initial subset generators -- output contain repeatation

---
+++ 12. subset II leadcode 90.py
@@ -0,0 +1,25 @@
+
+
+# with for loop
+all_sub = []
+def subsets1(l, index=0, temp = []):
+    
+    all_sub.append(temp[:])
+    for i in range(index, len(l)):
+        temp.append(l[i])
+        subsets1(l, i+1, temp)
+        temp.pop()
+
+
+# include - non include
+all_sub = []
+def subsets2(l, index=0, path=[]):
+    # not include
+    subsets(l, index+1, path)
+    
+    path.append(l[i])
+    subsets2(l, index+1, path)
+    path.pop()
+
+subsets1(['a', 'b', 'b', 'c'])
+print(all_sub)

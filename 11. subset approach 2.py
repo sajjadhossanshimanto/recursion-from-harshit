750002af8e39386c034504aafc4c851be976202b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-02 21:50:04 +0600

    leadcode submission

--- 11. subset approach 2.py
+++ 11. subset approach 2.py
@@ -14,3 +14,30 @@ def subsets(l, index=0, temp=[]):
 
 subsets(["a", 'b', 'c'])
 print(all_subs)
+
+
+#%%
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        all_subs = []
+        def backtrack(index=0, temp=[]):
+            # if index>len(l):
+            #     return
+
+            all_subs.append(temp[:])
+            for i in range(index, len(nums)):
+                temp.append(nums[i])
+                backtrack(i+1, temp)
+                temp.pop()
+        
+        backtrack()
+        return all_subs
+
+# %%
+Solution().subsets([1, 2, 3])
+# %%

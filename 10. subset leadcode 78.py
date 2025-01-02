9786b2ee16b0ff4d560096bf3efeb67ec93e1192
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-02 13:17:09 +0600

    dynamic programming approach

--- 10. subset leadcode 78.py
+++ 10. subset leadcode 78.py
@@ -15,5 +15,19 @@ def subsets(l, index=0, path=[]):
     subsets(l, index+1, path)
     path.pop()
 
+
+# dp approch for generating sub sets
+# time complexity is the same
+# but offer a better space complexity of 2^n instade of n.2^n
+def subsets2(l):
+    sub = [[]]
+    for ele in l:
+        for j in range(len(sub)):# trick to overcome sub[:]
+            sub.append(sub[j]+[ele])
+    
+    return sub
+
+
 subsets(['a', 'b', 'c'])
 print(sub)
+print(subsets2(['a', 'b', 'c']))

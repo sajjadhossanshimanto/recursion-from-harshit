574b2dec622ccc203932400d4234e7d02da1182f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-03 14:45:58 +0600

    better time complexity

--- 18. permutations 46.py
+++ 18. permutations 46.py
@@ -6,19 +6,33 @@ https://leetcode.com/problems/permutations/
 from typing import List
 
 
+'''
+permutation of any number is n!
+- array of length 3 will have 3! permutations
+- so the callback stack is of n!
+
+cloning an array taken O(n)
+is included takes O(n)
+
+total = n*n*n!
+we can improve the is included checking
+- 
+'''
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def backtrack(path=[]):
+        def backtrack(path=[], visit = [0]*len(nums)):
             if len(path)==len(nums):
                 res.append(path[:])
             
             for i in range(len(nums)):
-                if nums[i] in path: continue
+                if visit[i]: continue
+                visit[i] = 1
 
                 path.append(nums[i])
                 backtrack(path)
                 path.pop()
+                visit[i] = 0
 
         backtrack()
         return res

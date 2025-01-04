e681e3946dc9d11b5613217e9c568e17fa07a086
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-04 20:33:18 +0600

    finally ac

--- 24. restore ip address 93.py
+++ 24. restore ip address 93.py
@@ -8,6 +8,11 @@ from typing import List
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         ips = []
+        def valid_ip(segment):
+            # master checks. len>1 will both 00 as wll as 0 followed bya nay num
+            if segment[0]=="0" and len(segment)>1: return False
+            return 0<=int(segment)<=255
+
         def backtrack(start=0, path=[]):
             if len(path)==4 and start>=len(s):
                 ips.append(".".join(path))
@@ -16,10 +21,8 @@ class Solution:
 
             for end in range(start+1, min(start+4, len(s)+1)):
                 path.append(s[start: end])
-                if 0<=int(path[-1])<=255:
-                    if int(path[-1])==0 or path[-1][0]!="0":
-                        backtrack(end, path)
-                        # continue# leading zero check
+                if valid_ip(path[-1]):
+                    backtrack(end, path)
                 path.pop()
 
         backtrack()
@@ -35,4 +38,7 @@ s.restoreIpAddresses("0000")
 # %%
 # ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
 s.restoreIpAddresses("101023")
+# %% corner case
+# ["0.10.0.10","0.100.1.0"]
+s.restoreIpAddresses("010010")
 # %%

9484ca32e8ed19fa6da3f1a8912ac5c241375ca5
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-04 15:14:23 +0600

    generating the end result but have some repeatation

--- 23. generate parentheses 22.py
+++ 23. generate parentheses 22.py
@@ -18,20 +18,15 @@ class Solution:
                 path.append("(")
                 backtrack(open_counter+1, closed_counter, True, path)
                 path.pop()
-            if opend:
-                if open_counter==n:
-                    c=0
-                    for _ in range(n-closed_counter):
-                        path.append(")")
-                        c+=1
-                        # on every close i need to backtrack
-                    backtrack(open_counter, closed_counter+c, False, path)
-                    for _ in range(c):
-                        path.pop()
-                else:
+            if open_counter>closed_counter:
+                c=0
+                for _ in range(open_counter-closed_counter):
                     path.append(")")
-                    backtrack(open_counter, closed_counter+1, False, path)
+                    c+=1
+                    backtrack(open_counter, closed_counter+c, False, path)
+                for _ in range(c):
                     path.pop()
+            
 
         backtrack()
         return res

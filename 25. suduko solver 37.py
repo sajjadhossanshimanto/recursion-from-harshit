ab279e7e392f1e97e9e0170ff0e72cbc4c4cb493
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-04 22:32:30 +0600

    basic implimentation

--- 25. suduko solver 37.py
+++ 25. suduko solver 37.py
@@ -10,9 +10,7 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         row, column = len(board), len(board[0])
-        def is_valid(x, y) -> List:
-            ele = board[x][y]
-            
+        def is_valid(x, y, ele) -> bool:
             # column checking
             for i in range(row):
                 if board[i][y]==ele: return False
@@ -27,10 +25,27 @@ class Solution:
                     if board[i][j] == ele: return False
             
             return True
+        
+        def next_pos(x, y):
+            if y<column: return x, y+1
+            if x<row: return x+1, 0
+            # else end of cell
 
-        def backtrack():
-            pass
+        def backtrack(x=0, y=0):# starting x, y
+            if x==row and y==column: return True
+            
+            for i in range(x, row):
+                for j in range(y, column):# code doesn't cover all cells
+                    if board[i][j]!=".": continue
 
+                    for num in range(1, 9+1):
+                        if not is_valid(i, j, str(num)): continue
+                        
+                        board[i][j] = str(num)
+                        if backtrack(*next_pos(i, j)): return True
+                        board[i][j] = "."
+
+        backtrack()
 
 
 s = Solution()
@@ -38,3 +53,4 @@ s = Solution()
 board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 s.solveSudoku(board)
 print(board)
+# %%

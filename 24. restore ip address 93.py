7e766c4dc717c5b14c2c0807caec6d8ec7eeda41
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-04 19:29:25 +0600

    basic implimentation

---
+++ 24. restore ip address 93.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode.com/problems/restore-ip-addresses/
+'''
+#%%
+from typing import List
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ips = []
+        def backtrack(start=0, path=[]):
+            if len(path)==4 and start>=len(s):
+                ips.append(".".join(path))
+                return
+            if len(path)>4: return
+
+            for end in range(start+1, start+4):
+                path.append(s[start: end])
+                if 0<=int(path[-1])<=255:
+                    # and path[-1][0]!="0"# TODO: check for leading zeros
+                    backtrack(end, path)
+                path.pop()
+
+        
+        backtrack()
+        return ips
+
+s=Solution()
+# %%
+# ["255.255.11.135","255.255.111.35"]
+s.restoreIpAddresses("25525511135")
+# %%
+# ["0.0.0.0"]
+s.restoreIpAddresses("0000")
+# %%
+# ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+s.restoreIpAddresses("101023")
+# %%
